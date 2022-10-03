2133625b8655aa9ca8184c226221d106fca13562
Michelelai <112725491+Michelelai@users.noreply.github.com>
2022-10-03 16:14:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+secret = 13
+guess = int(input("Enter your number and push enter: "))
+
+if guess == secret:
+    print("Congratulations, you guessed the number!!")
+else:
+    print("Sorry, the number is wrong...")
